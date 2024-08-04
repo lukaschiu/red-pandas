a9548c0ac0ca2082f96a0cfee59ca7c1e1d1294e
Lukas Chiu <64233206+lukaschiu@users.noreply.github.com>
2024-08-04 00:07:30 +0000

    Red Pandas Update

--- places/countries.js
+++ places/countries.js
@@ -1,7 +1,7 @@
 // Use ISO alpha-3 country codes, see e.g. https://en.wikipedia.org/wiki/ISO_3166-1_alpha-3
 COUNTRIES =
 {
-    //USA: {fillKey: 'visited'},
+    USA: {fillKey: 'visited'},
 }
 
 // index.html only checks for the existence of ISO alpha-3 country codes, the values are not used.
